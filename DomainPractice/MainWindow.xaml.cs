48b9fd00ced4426581bb43c29b06f758f9d3bbc5
Владислав Горохов <gbg.0101@mail.ru>
2019-06-11 15:46:42 +0600

    Добавлены файлы в bin

--- DomainPractice/MainWindow.xaml.cs
+++ DomainPractice/MainWindow.xaml.cs
@@ -18,72 +18,28 @@ using System.Windows.Shapes;
 
 namespace DomainPractice
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        private string _url;
-
         public MainWindow()
         {
             InitializeComponent();
             progressBar.IsIndeterminate = false;
-
-            _url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=time";
         }
 
         private void DownloadButtonClick(object sender, RoutedEventArgs e)
         {
             progressBar.IsIndeterminate = true;
-            WebClient webClient = new WebClient();
-            
-            string link = @"ftp://speedtest:speedtest@ftp.otenet.gr/test100Mb.db";
-            string downloadFileName = System.IO.Path.GetFileName("1.db");
-            webClient.DownloadFileAsync(new Uri(link), @"C:\Users\гороховв.CORP\Desktop\" + downloadFileName);
+
+
             progressBar.IsIndeterminate = false;
         }
 
-    
-        private async void CountButtonClick(object sender, RoutedEventArgs e)
+        private void CountButtonClick(object sender, RoutedEventArgs e)
         {
-            progressBar.Value = 10;
+            progressBar.IsIndeterminate = true;
 
-            string json = "";
 
-            WebClient webClient = new WebClient();
-
-            Stream stream = await webClient.OpenReadTaskAsync(new Uri(_url));
-            using (var reader = new StreamReader(stream))
-            {
-                string line = "";
-                while ((line = reader.ReadLine()) != null)
-                {
-                    json += line;
-                }
-            }
-            progressBar.Value = 40;
-
-            EarthquakeService earthquakeService = JsonConvert.DeserializeObject<EarthquakeService>(json);
-
-            progressBar.Value = 80;
-
-            int amount = 0;
-
-            foreach (var earthquake in earthquakeService.Features)
-            {
-                if (earthquake.Information.Magnitude >= 1)
-                {
-                    amount++;
-                }
-            }
-
-            progressBar.Value = 100;
-
-            MessageBox.Show(amount.ToString());
-
-            progressBar.Value = 0;
+            progressBar.IsIndeterminate = false;
         }
-
     }
 }
